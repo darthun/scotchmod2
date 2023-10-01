7c0566107ecc36e1ddc33b8c453c86275cc6bfe9
darthun <62441274+darthun@users.noreply.github.com>
2023-10-01 19:54:06 -0400

    finished JEI integration

--- src/main/java/net/darthun/scotchmod/compat/BarleySteepRecipeCategory.java
+++ src/main/java/net/darthun/scotchmod/compat/BarleySteepRecipeCategory.java
@@ -1,10 +1,10 @@
 package net.darthun.scotchmod.compat;
-
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import net.darthun.scotchmod.ScotchMod;
@@ -13,26 +13,27 @@ import net.darthun.scotchmod.recipe.BarleySteepRecipe;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
-/*
+
 public class BarleySteepRecipeCategory implements IRecipeCategory<BarleySteepRecipe> {
-    public static final ResourceLocation UID= new ResourceLocation(ScotchMod.MOD_ID,"barley_steep");
+    public static final ResourceLocation UID = new ResourceLocation(ScotchMod.MOD_ID, "barley_steep");
     public static final ResourceLocation TEXTURE = new ResourceLocation(ScotchMod.MOD_ID,
             "textures/gui/barley_steep_gui.png");
 
-    public final RecipeType BARLEY_STEEP_RECIPE =
-            new RecipeType(UID,);
+    public static final RecipeType<BarleySteepRecipe> BARLEY_STEEP_TYPE =
+            new RecipeType<>(UID, BarleySteepRecipe.class);
 
     private final IDrawable background;
     private final IDrawable icon;
 
     public BarleySteepRecipeCategory(IGuiHelper helper) {
-        this.background = helper.createDrawable(TEXTURE,0,0,176,85);
-        this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK,new ItemStack(ModBlocks.BARLEY_STEEP_BLOCK.get()));
+        this.background = helper.createDrawable(TEXTURE, 0, 0, 176, 85);
+        this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ModBlocks.BARLEY_STEEP_BLOCK.get()));
     }
 
+
     @Override
-    public RecipeType getRecipeType() {
-        return null;
+    public RecipeType<BarleySteepRecipe> getRecipeType() {
+        return BARLEY_STEEP_TYPE;
     }
 
     @Override
@@ -51,8 +52,9 @@ public class BarleySteepRecipeCategory implements IRecipeCategory<BarleySteepRec
     }
 
     @Override
-    public void setRecipe(IRecipeLayoutBuilder builder, Object recipe, IFocusGroup focuses) {
+    public void setRecipe(IRecipeLayoutBuilder builder, BarleySteepRecipe recipe, IFocusGroup focuses) {
+        builder.addSlot(RecipeIngredientRole.INPUT, 54, 28).addIngredients(recipe.getIngredients().get(0));
 
+        builder.addSlot(RecipeIngredientRole.OUTPUT, 111, 28).addItemStack(recipe.getResultItem(null));
     }
 }
-*/
--- src/main/java/net/darthun/scotchmod/compat/JEIScotchModPlugin.java
+++ src/main/java/net/darthun/scotchmod/compat/JEIScotchModPlugin.java
@@ -1,8 +1,17 @@
 package net.darthun.scotchmod.compat;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IGuiHandlerRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
 import net.darthun.scotchmod.ScotchMod;
+import net.darthun.scotchmod.recipe.BarleySteepRecipe;
+import net.darthun.scotchmod.screen.BarleySteepScreen;
+import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.RecipeManager;
+
+import java.util.List;
 
 @JeiPlugin
 public class JEIScotchModPlugin implements IModPlugin {
@@ -10,5 +19,23 @@ public class JEIScotchModPlugin implements IModPlugin {
     @Override
     public ResourceLocation getPluginUid() {
         return new ResourceLocation(ScotchMod.MOD_ID,"jei_plugin");
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        registration.addRecipeCategories(new BarleySteepRecipeCategory(
+                        registration.getJeiHelpers().getGuiHelper()));
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        RecipeManager recipeManager = Minecraft.getInstance().level.getRecipeManager();
+        List<BarleySteepRecipe> barleySteepRecipes = recipeManager.getAllRecipesFor(BarleySteepRecipe.Type.INSTANCE);
+        registration.addRecipes(BarleySteepRecipeCategory.BARLEY_STEEP_TYPE,barleySteepRecipes);
+    }
+
+    @Override
+    public void registerGuiHandlers(IGuiHandlerRegistration registration) {
+        registration.addRecipeClickArea(BarleySteepScreen.class,80,20,30,30,BarleySteepRecipeCategory.BARLEY_STEEP_TYPE);
     }
 }
